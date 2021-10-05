478d0326af4b05884dcfc47f4d5506f9e153d84c
David Gurule <davidgurulejr@gmail.com>
2021-10-05 13:55:26 -0600

    The enter your date is now responding with the api data

--- script.js
+++ script.js
@@ -17,11 +17,10 @@ Get the "Number of Objects" to add up to the fetch Data
 fetch
 */
 const todaysDate = new Date();
-// console.log(todaysDate);
+console.log(todaysDate);
 
 let todaysDateFormatted = `${todaysDate.getFullYear()}-${todaysDate.getMonth()+1}-${todaysDate.getDate()}`;
-
-// console.log(todaysDateFormatted);
+console.log(todaysDateFormatted);
 
 // https://phoenixnap.com/kb/how-to-get-the-current-date-and-time-javascript
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
@@ -53,29 +52,61 @@ window.addEventListener('load', (event) => {
   //This is where the number API needs to live
 })
 
-//------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
 const fetchUserData = (userDate) => {
   fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${userDate}&end_date=${userDate}&api_key=0eJcYmnO2iE9jsyArre7QyVJRPWqTsaT7BGUaLHX`)
     .then((response) => { return response.json() })
     .then((responseJSON) => {
-    console.log(responseJSON); 
+    // console.log(responseJSON); 
+    
+  
+    // --------------------------------------------------------------------------
+      // call on the summary
+      // create a div
+      // append the div to the summary
+      // add content to the div (name, size of object, speed, distance from earth)
+    // --------------------------------------------------------------------------
     const summary = document.querySelector('summary');
-    summary.innerText = responseJSON.element_count;
+    const collisionObject = responseJSON.near_earth_objects[userDate];  
+    console.log(collisionObject); 
+    
+    for (let i=0; i <collisionObject.length; i++) {
+    
+     
+    const collName = document.createElement('p');
+    const collSizeMax = document.createElement('p');
+    const collSizeMin = document.createElement('p');
+    const collSpeed = document.createElement('p');
+    // const collDist = document.createElement('p');
+
+    
+
+    collName.innerText = collisionObject[i].name;
+    collSizeMax.innerText = collisionObject[i].estimated_diameter.feet.estimated_diameter_max;
+    collSizeMin.innerText = collisionObject[i].estimated_diameter.feet.estimated_diameter_min;
+    collSpeed.innerText = collisionObject[i].close_approach_data[0].relative_velocity].miles_per_hour;
+    // collDist.innerText = collisionObject[i].close_approach_data[miss_distance].miles;
+    
+    
+    
+    summary.appendChild(collName);
+    summary.appendChild(collSizeMax);
+    summary.appendChild(collSizeMin);
+    summary.appendChild(collSpeed);
+    // summary.appendChild(collDist);
+    }
 
 
+  });
+  }
+  //   for (let i=0; i < collisionObject.length; i++) {  
+  //       // console.log(collisionObject[i].name);
 
-    })
-}
-
-
-
-
+const submit = document.querySelector('#submit');
 submit.addEventListener('click', (event) => {
     event.preventDefault();
-    // console.log('button was pressed');
-    const submit = document.querySelector('#submit');
+    console.log('button was pressed');
     let userDate = document.querySelector('#userDateInput').value;
-    fetchUserData(userDate);
-   
-  });
+    fetchUserData(userDate)
+});
